5173a48c458dfa6059fd01891538b26ae7c582fb
vivek saud <viveksaud3@gmail.com>
2022-11-26 17:33:36 +0545

    Changed the branch to mi_card_flutter

--- 02_MiCard-A_Single_Screen_Personal_Business_Card/mi_card_flutter/lib/main.dart
+++ 02_MiCard-A_Single_Screen_Personal_Business_Card/mi_card_flutter/lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-     debugShowCheckedModeBanner: false, // to hide debug mode banner
-     home: Scaffold(
+      home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
