ad9eaed1a37af758d24623beee494a04d6e206e4
vivek saud <viveksaud3@gmail.com>
2022-11-26 13:24:09 +0545

    Scaffolding a flutter app

--- 01_I_am_rich/01_i_am_rich_flutter/lib/main.dart
+++ 01_I_am_rich/01_i_am_rich_flutter/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text('Hello World!!!'))));
+   runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false, // to hide debug mode banner
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I Am Rich"),
+          backgroundColor: Colors.purple[900],
+        ),
+        backgroundColor: Colors.deepPurple[400],
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://media.istockphoto.com/id/1382204548/photo/lord-shiva-hindu-god-stock-photo.jpg?b=1&s=170667a&w=0&k=20&c=o8IynHJ2VhwIhzQXBR9TJK3wBt-EchouwPNkVNcOh8Q='),
+          ),
+        ),
+      ),
+    ),
+  );
 }
 
 
