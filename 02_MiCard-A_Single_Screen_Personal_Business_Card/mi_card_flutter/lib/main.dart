54777c72bf80c79dad74108249333665b1ef3100
vivek saud <viveksaud3@gmail.com>
2022-11-26 13:57:51 +0545

    02-starting file

---
+++ 02_MiCard-A_Single_Screen_Personal_Business_Card/mi_card_flutter/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+     debugShowCheckedModeBanner: false, // to hide debug mode banner
+     home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
