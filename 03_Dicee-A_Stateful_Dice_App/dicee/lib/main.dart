d5a7673f42163749c86483653b456be7da245adb
vivek saud <viveksaud3@gmail.com>
2022-11-30 20:01:25 +0545

    Adding Gesture Detection with Flutter Button widget

--- 03_Dicee-A_Stateful_Dice_App/dicee/lib/main.dart
+++ 03_Dicee-A_Stateful_Dice_App/dicee/lib/main.dart
@@ -25,17 +25,31 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(// it is used to fix the overflow 
-          flex: 1,//flex is just a ratio given to the expanded item// by default, its value is 1, so expanded item are equal in size
-          child: Image.asset('images/dice1.png'), //Image(image: AssetImage('images/dice1.png'))
-        ),
-        Expanded(
-          flex: 1,
-          child: Image.asset('images/dice1.png'), //Image(image: AssetImage('images/dice1.png'))
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // it is used to fix the overflow
+            // flex: 1,//flex is just a ratio given to the expanded item// by default, its value is 1, so expanded item are equal in size
+            child: TextButton(
+                //button has already padding of 16 px on left and right
+                onPressed: () {
+                  print('Left Button got clicked!');
+                },
+                child: Image.asset(
+                    'images/dice1.png')), //Image(image: AssetImage('images/dice1.png'))
+          ),
+          Expanded(
+            // flex: 1,
+            child: TextButton(
+              onPressed: () {
+                print('Right Button got clicked!');
+              },
+              child: Image.asset('images/dice1.png'),
+            ), //Image(image: AssetImage('images/dice1.png'))
+          ),
+        ],
+      ),
     );
   }
 }
